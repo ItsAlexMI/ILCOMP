1cf0233fc1630f9d886eecacaee77a13a9f0263e
ItsAlexMI <alexmartinezirias2004@gmail.com>
2024-11-03 22:45:39 -0600

    🚧

--- App/urls.py
+++ App/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), 
+    path('titulo_otorgado', views.titulo_otorgado, name='Titulo_otorgado'),
+    path('certificacion_titulo', views.certificacion_titulo, name='Certificacion_titulo'),
+    path('publicacion_gaceta', views.publicacion_gaceta, name='Publicacion_gaceta'),
+    path('acta_culminacion', views.acta_culminacion, name='Acta_culminacion'),
+    path('plan_estudios', views.plan_estudios, name='Plan_estudios'),
     path('login', views.login, name='login'),
 ]
--- App/views.py
+++ App/views.py
@@ -9,3 +9,17 @@ def home(request):
 def login(request):
     return render(request, 'login.html')
     
+def titulo_otorgado(request):
+    return render(request, 'titulo_otorgado.html')
+
+def certificacion_titulo(request):
+    return render(request, 'certificacion_titulo.html')
+
+def publicacion_gaceta(request):
+    return render(request, 'publicacion_gaceta.html')
+
+def acta_culminacion(request):
+    return render(request, 'acta_culminacion.html')
+
+def plan_estudios(request):
+    return render(request, 'plan_estudios.html')
