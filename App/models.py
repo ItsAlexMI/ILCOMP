4979d07a8794a82415dc0d5f3efff5eee6446884
MiguelBojorge <mabojorgeb@gmail.com>
2024-11-17 20:43:05 -0600

    🚧

--- App/models.py
+++ App/models.py
@@ -1,4 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.db import models
 
+class docente(models.Model):
+    nombre = models.CharField(max_length=200)
+    username = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nombre
