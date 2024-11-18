92a3c29dd97e37d28ba23d9298a381fca11a7f93
MiguelBojorge <mabojorgeb@gmail.com>
2024-11-18 00:33:39 -0600

    :construction:

--- App/models.py
+++ App/models.py
@@ -14,3 +14,36 @@ class docente(models.Model):
     class Meta:
         db_table = "docente"
         managed=False
+
+
+class Plan_encabezado(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        db_table = "plan_encabezado"
+        managed = False
+
+class Alumnos(models.Model):
+    carnet = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=200)
+    ano_ingreso = models.IntegerField(default=0)
+    username_Alumno = models.CharField(max_length=100, unique=True)
+    id_plan_encabezado = models.ForeignKey(Plan_encabezado,on_delete=models.CASCADE)
+    usernameAlumno = models.CharField(max_length=100, unique=True)
+
+
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        db_table = "alumnos"
+        managed = False
+
+
+
+
